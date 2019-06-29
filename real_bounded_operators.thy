dc1113be5853d76653a50cbb41d2f68f55304b2d
José Manuel Rodríguez Caballero <37361666+josephcmac@users.noreply.github.com>
2019-06-29 12:10:39 +0300

    instantiation of real_bounded_operators

--- real_bounded_operators.thy
+++ real_bounded_operators.thy
@@ -40,12 +40,44 @@ is \<open>\<lambda> f. \<lambda> x. f x\<close>.
 
 instantiation real_operator :: (real_vector, real_vector) \<open>real_vector\<close>
 begin
-lift_definition zero_real_operator :: \<open>('a, 'b) real_operator\<close> is \<open>\<lambda> _. 0\<close>
+lift_definition uminus_real_operator :: \<open>('a, 'b) real_operator \<Rightarrow> ('a, 'b) real_operator\<close>
+is \<open>\<lambda> f. ( \<lambda> x. -f x)\<close>
+  by (rule Real_Vector_Spaces.real_vector.linear_compose_neg)
+lift_definition zero_real_operator :: \<open>('a, 'b) real_operator\<close> is \<open>\<lambda> _::'a. 0::'b\<close>
   by (rule Real_Vector_Spaces.real_vector.linear_zero)
+lift_definition minus_real_operator :: \<open>('a, 'b) real_operator \<Rightarrow> ('a, 'b) real_operator \<Rightarrow> ('a, 'b) real_operator\<close>
+is \<open>\<lambda> f. \<lambda> g. ( \<lambda> x. f x - g x)\<close>
+  by (rule Real_Vector_Spaces.real_vector.module_hom_sub)
+lift_definition plus_real_operator :: \<open>('a, 'b) real_operator \<Rightarrow> ('a, 'b) real_operator \<Rightarrow> ('a, 'b) real_operator\<close>
+is \<open>\<lambda> f. \<lambda> g. ( \<lambda> x. f x + g x)\<close>
+  by (rule Real_Vector_Spaces.real_vector.linear_compose_add)
+lift_definition scaleR_real_operator :: \<open>real \<Rightarrow> ('a, 'b) real_operator \<Rightarrow> ('a, 'b) real_operator\<close>
+is \<open>\<lambda> c. \<lambda> f. (\<lambda> x. c *\<^sub>R (f x))\<close>
+  by (rule Real_Vector_Spaces.real_vector.linear_compose_scale_right)
 
-instance..
-
+instance
+  apply intro_classes
+  apply transfer
+  apply auto
+  apply transfer
+  apply auto
+  apply transfer
+  apply auto
+  apply transfer
+  apply auto
+  apply transfer
+  apply auto
+  apply transfer
+  apply (simp add: scaleR_add_right)
+  apply transfer
+  apply (simp add: scaleR_add_left)
+  apply transfer
+  apply simp
+  apply transfer
+  apply auto
+  done
 end
+
 
 section \<open>Bounded linear operators\<close>
 
@@ -59,14 +91,44 @@ setup_lifting type_definition_real_bounded
 lift_definition ev_real_bounded :: \<open>('a::real_normed_vector, 'b::real_normed_vector) real_bounded \<Rightarrow> 'a \<Rightarrow> 'b\<close> 
 is \<open>\<lambda> f. \<lambda> x. ev_real_operator f x\<close>.
 
-instantiation real_bounded :: (real_normed_vector, real_normed_vector) \<open>zero\<close>
+instantiation real_bounded :: (real_normed_vector, real_normed_vector) \<open>real_vector\<close>
 begin
-lift_definition zero_real_bounded :: \<open>('a, 'b) real_bounded\<close> is \<open>0::('a,'b) real_operator\<close>
+  
+lift_definition uminus_real_bounded :: \<open>('a::real_normed_vector, 'b::real_normed_vector) real_bounded \<Rightarrow> ('a::real_normed_vector, 'b::real_normed_vector) real_bounded\<close>
+is \<open>\<lambda> f. -f\<close>
+  apply transfer
+  using bounded_linear_minus bounded_linear.axioms(2) bounded_linear.intro by auto
+
+lift_definition zero_real_boudned :: \<open>('a::real_normed_vector, 'b::real_normed_vector) real_bounded\<close>
+ is \<open>0\<close>
   apply transfer
   by (simp add: bounded_linear.axioms(2))
-instance..
-end
 
+lift_definition minus_real_bounded :: \<open>('a::real_normed_vector, 'b::real_normed_vector) real_bounded \<Rightarrow> ('a::real_normed_vector, 'b::real_normed_vector) real_bounded \<Rightarrow> ('a::real_normed_vector, 'b::real_normed_vector) real_bounded\<close>
+is \<open>\<lambda> f. \<lambda> g. f - g\<close>
+  apply transfer
+  using bounded_linear_sub bounded_linear_def by blast
+
+lift_definition plus_real_bounded :: \<open>('a::real_normed_vector, 'b::real_normed_vector) real_bounded \<Rightarrow> ('a::real_normed_vector, 'b::real_normed_vector) real_bounded \<Rightarrow> ('a::real_normed_vector, 'b::real_normed_vector) real_bounded\<close>
+is \<open>\<lambda> f g.  f + g\<close>
+  apply transfer
+  using bounded_linear_add bounded_linear_def by blast
+
+lift_definition scaleR_real_bounded :: \<open>real \<Rightarrow> ('a::real_normed_vector, 'b::real_normed_vector) real_bounded \<Rightarrow> ('a::real_normed_vector, 'b::real_normed_vector) real_bounded\<close>
+is \<open>\<lambda> c. \<lambda> f. c *\<^sub>R f\<close>
+  apply transfer
+  using bounded_linear_const_scaleR bounded_linear_def by blast
+
+instance
+  apply intro_classes
+  apply transfer
+  apply auto
+  apply transfer
+  apply auto
+  apply transfer
+  sorry    
+
+end
 
 
 end
