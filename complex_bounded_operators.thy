267eefc63708f96925e5d64df0fa2bf30519c9f6
José Manuel Rodríguez Caballero <37361666+josephcmac@users.noreply.github.com>
2019-06-29 08:04:22 +0300

    new definition of complex bounded operator

--- complex_bounded_operators.thy
+++ complex_bounded_operators.thy
@@ -21,9 +21,15 @@ theory complex_bounded_operators
   imports 
     "HOL-ex.Sketch_and_Explore"
     real_bounded_operators
+    Complex_Vector_Spaces
 begin
 
 
-
+typedef (overloaded) ('a::complex_normed_vector, 'b::complex_normed_vector) complex_bounded
+= \<open>{f :: ('a, 'b) real_bounded. \<forall> c. \<forall> x. (Rep_real_bounded f) (c *\<^sub>C x) = c *\<^sub>C ((Rep_real_bounded f) x) }\<close>
+  apply transfer
+  using zero_real_bounded_def 
+  apply auto
+  using bounded_linear_zero by fastforce
 
 end
