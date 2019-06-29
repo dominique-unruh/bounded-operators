2eab1301118252cd746884bd0f1a2abb04a07ca5
José Manuel Rodríguez Caballero <37361666+josephcmac@users.noreply.github.com>
2019-06-29 09:31:23 +0300

    redefinition of bounded_operators

--- complex_bounded_operators.thy
+++ complex_bounded_operators.thy
@@ -1,8 +1,8 @@
-(*  Title:      bounded-Operators/real_bounded_operators.thy
+(*  Title:      bounded-Operators/complex_bounded_operators.thy
     Author:     Dominique Unruh, University of Tartu
     Author:     Jose Manuel Rodriguez Caballero, University of Tartu
 
-Operators between real vector spaces.
+Operators between complex vector spaces.
 
 References:             
 
@@ -24,12 +24,29 @@ theory complex_bounded_operators
     Complex_Vector_Spaces
 begin
 
-
 typedef (overloaded) ('a::complex_normed_vector, 'b::complex_normed_vector) complex_bounded
-= \<open>{f :: ('a, 'b) real_bounded. \<forall> c. \<forall> x. (Rep_real_bounded f) (c *\<^sub>C x) = c *\<^sub>C ((Rep_real_bounded f) x) }\<close>
+= \<open>{f :: ('a, 'b) real_bounded. \<forall> c. \<forall> x. ev_real_bounded f (c *\<^sub>C x) = c *\<^sub>C (ev_real_bounded f x) }\<close>
   apply transfer
-  using zero_real_bounded_def 
-  apply auto
-  using bounded_linear_zero by fastforce
+proof -
+  obtain rr :: "('a, 'b) real_bounded \<Rightarrow> ('a, 'b) real_operator" where
+    f1: "\<forall>r. rr r \<in> Collect bounded_real_operator \<and> Abs_real_bounded (rr r) = r"
+by (metis (no_types) Abs_real_bounded_cases)
+obtain cc :: "('a, 'b) real_operator \<Rightarrow> complex" and aa :: "('a, 'b) real_operator \<Rightarrow> 'a" where
+  f2: "\<forall>r. ((\<forall>c a. ev_real_operator r (c *\<^sub>C a) = c *\<^sub>C ev_real_operator r a) \<and> bounded_real_operator r \<or> \<not> bounded_real_operator r \<or> ev_real_operator r (cc r *\<^sub>C aa r) \<noteq> cc r *\<^sub>C ev_real_operator r (aa r)) \<and> (bounded_real_operator r \<and> (\<forall>c a. ev_real_operator r (c *\<^sub>C a) = c *\<^sub>C ev_real_operator r a) \<or> (\<exists>c a. ev_real_operator r (c *\<^sub>C a) \<noteq> c *\<^sub>C ev_real_operator r a) \<or> \<not> bounded_real_operator r)"
+  by moura
+  obtain bb :: "('a, 'b) real_operator \<Rightarrow> 'a \<Rightarrow> 'b" where
+    f3: "\<forall>r. ev_real_operator r = bb r"
+    by metis
+  have "0 = rr 0"
+    using f1 by (metis (no_types) Abs_real_bounded_inverse cr_real_bounded_def zero_real_bounded.transfer)
+  then have "\<exists>r. bb r (cc r *\<^sub>C aa r) = cc r *\<^sub>C bb r (aa r) \<and> bounded_real_operator r"
+    using f3 f1 by (metis complex_vector.scale_eq_0_iff ev_real_operator.rep_eq mem_Collect_eq zero_real_operator.rep_eq)
+  then show "\<exists>r\<in>{r. bounded_real_operator r}. r \<in> {r. (\<forall>c a. ev_real_operator r (c *\<^sub>C (a::'a)) = c *\<^sub>C (ev_real_operator r a::'b)) \<and> bounded_real_operator r}"
+    using f3 f2 by auto
+qed
+
+lift_definition ev_complex_bounded :: \<open>('a::complex_normed_vector, 'b::complex_normed_vector) real_bounded \<Rightarrow> 'a \<Rightarrow> 'b\<close> 
+is \<open>\<lambda> f. \<lambda> x. ev_real_bounded f x\<close>.
+
 
 end
--- real_bounded_operators.thy
+++ real_bounded_operators.thy
@@ -23,17 +23,47 @@ theory real_bounded_operators
     Operator_Norm_Plus
 begin
 
+section \<open>Linear operators\<close>
+(* The operators may be unbounded *)
+
+typedef (overloaded) ('a::real_vector, 'b::real_vector) real_operator
+= \<open>{ f::'a\<Rightarrow>'b. linear f}\<close>
+  using linear_zero by blast
+
+setup_lifting type_definition_real_operator
+
+lift_definition bounded_real_operator :: \<open>('a::real_normed_vector, 'b::real_normed_vector) real_operator \<Rightarrow> bool\<close> 
+is \<open>\<lambda> f. bounded_linear_axioms f\<close>.
+
+lift_definition ev_real_operator :: \<open>('a::real_vector, 'b::real_vector) real_operator \<Rightarrow> 'a \<Rightarrow> 'b\<close> 
+is \<open>\<lambda> f. \<lambda> x. f x\<close>.
+
+instantiation real_operator :: (real_vector, real_vector) \<open>real_vector\<close>
+begin
+lift_definition zero_real_operator :: \<open>('a, 'b) real_operator\<close> is \<open>\<lambda> _. 0\<close>
+  by (rule Real_Vector_Spaces.real_vector.linear_zero)
+
+instance..
+
+end
+
+section \<open>Bounded linear operators\<close>
+
 typedef (overloaded) ('a::real_normed_vector, 'b::real_normed_vector) real_bounded
-= \<open>{ f::'a\<Rightarrow>'b. bounded_linear f}\<close>
-  using bounded_linear_zero by blast
+= \<open>{ f::('a, 'b) real_operator. bounded_real_operator f}\<close>
+  apply transfer
+  using bounded_linear.axioms(2) bounded_linear_zero module_hom_zero by blast
 
 setup_lifting type_definition_real_bounded
 
+lift_definition ev_real_bounded :: \<open>('a::real_normed_vector, 'b::real_normed_vector) real_bounded \<Rightarrow> 'a \<Rightarrow> 'b\<close> 
+is \<open>\<lambda> f. \<lambda> x. ev_real_operator f x\<close>.
 
 instantiation real_bounded :: (real_normed_vector, real_normed_vector) \<open>zero\<close>
 begin
-lift_definition zero_real_bounded :: \<open>('a, 'b) real_bounded\<close> is \<open>\<lambda> _::'a. 0::'b\<close>
-  by (rule bounded_linear_zero)
+lift_definition zero_real_bounded :: \<open>('a, 'b) real_bounded\<close> is \<open>0::('a,'b) real_operator\<close>
+  apply transfer
+  by (simp add: bounded_linear.axioms(2))
 instance..
 end
 
