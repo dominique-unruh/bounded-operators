f64c6dd1210d41d534a8b4e7bb83ef736f0432f0
José Manuel Rodríguez Caballero <37361666+josephcmac@users.noreply.github.com>
2019-07-01 04:34:59 +0300

    non-boolean reformulation of Banach-Steinhaus theorem
     The Banach-Steinhaus theorem is interpreted as the fact that a type is inhabited

--- real_bounded_operators.thy
+++ real_bounded_operators.thy
@@ -19,7 +19,7 @@ References:
 theory real_bounded_operators
   imports 
     "HOL-ex.Sketch_and_Explore"
-    Operator_Norm_Plus
+    SEQ_bounded_operators
 begin
 
 
@@ -138,6 +138,125 @@ instance
 end
 
 
+subsection \<open>Lifting Banach-Steinhaus\<close>
+
+typedef (overloaded) ('a::real_normed_vector, 'b::real_normed_vector) real_bounded_SEQ_bounded_pointwise
+= \<open>{f :: nat \<Rightarrow> ('a, 'b) real_bounded. \<forall> x::'a. bdd_above { norm ( ev_real_bounded (f n) x ) | n. True } }\<close>
+  apply transfer
+proof
+  show \<open>(\<lambda> n::nat. (\<lambda> _::'a. 0::'b)) \<in> {f. (\<forall>x. bdd_above {norm (f n x) |n. True}) \<and> pred_fun top bounded_linear f}\<close>
+    apply auto.
+  show \<open>(\<lambda> n::nat. (\<lambda> _::'a. 0::'b)) \<in> Collect (pred_fun top bounded_linear)\<close>
+    apply auto.
+qed
+
+setup_lifting type_definition_real_bounded_SEQ_bounded_pointwise
+
+lift_definition index_real_bounded :: 
+\<open>('a::real_normed_vector, 'b::real_normed_vector) real_bounded_SEQ_bounded_pointwise \<Rightarrow> nat 
+\<Rightarrow> ('a, 'b) real_bounded\<close> is \<open>\<lambda> f::nat \<Rightarrow> ('a, 'b) real_bounded. \<lambda> n::nat. f n\<close>.
+
+lift_definition norm_SEQ_real_bounded :: 
+\<open>('a::real_normed_vector, 'b::real_normed_vector) real_bounded_SEQ_bounded_pointwise \<Rightarrow> (nat 
+\<Rightarrow> real)\<close> is \<open>\<lambda> f::nat \<Rightarrow> ('a, 'b) real_bounded. \<lambda> n::nat. norm (f n)\<close>.
+
+
+typedef (overloaded) ('a::metric_space) bounded_SEQ
+= \<open>{f :: nat \<Rightarrow> 'a.  bounded { f n | n. True } }\<close>
+proof-
+  have \<open>\<exists> x. x \<in> (UNIV :: 'a set)\<close>
+    by simp
+  then obtain x where \<open>x \<in> (UNIV :: 'a set)\<close>
+    by blast
+  hence \<open>bounded {x}\<close>
+    by simp    
+  hence \<open>bounded {(\<lambda> _::nat. x) n |n. True}\<close>
+    by simp
+  hence \<open>(\<lambda> _::nat. x) \<in> {f. bounded {f n |n. True}}\<close>
+    by simp
+  thus ?thesis 
+    by meson 
+qed
+
+setup_lifting type_definition_bounded_SEQ
+
+corollary Banach_Steinhaus_coro:
+  fixes f :: \<open>nat \<Rightarrow> ('a::{banach,perfect_space} \<Rightarrow> 'b::real_normed_vector)\<close>
+  shows \<open>\<forall>n. bounded_linear (f n)
+\<Longrightarrow> \<forall> x. bdd_above {norm ((f n) x) | n. True}
+\<Longrightarrow> bounded {onorm (f n) | n. True}\<close>
+proof-
+  assume \<open>\<forall>n. bounded_linear (f n)\<close>
+  hence  \<open>\<And> n. bounded_linear (f n)\<close>
+    by blast
+  assume \<open>\<forall> x. bdd_above {norm ((f n) x) | n. True}\<close>
+  hence \<open>\<And> x. \<exists> M. \<forall> n.  norm ((f n) x) \<le> M\<close>
+    using bdd_above_def
+    by (metis (mono_tags) mem_Collect_eq)
+  have  \<open>\<exists> M. \<forall> n. onorm (f n) \<le> M\<close>
+    using  \<open>\<And> n. bounded_linear (f n)\<close> \<open>\<And> x. \<exists> M. \<forall> n.  norm ((f n) x) \<le> M\<close>  
+    by (rule Banach_Steinhaus)
+  then obtain M where \<open>\<forall> n. onorm (f n) \<le> M\<close> by blast
+  have \<open>y\<in>{onorm (f n) |n. True} \<Longrightarrow> \<bar>y\<bar> \<le> M\<close>
+    for y
+  proof-
+    assume \<open>y\<in>{onorm (f n) |n. True}\<close>
+    hence \<open>\<exists> n. y = onorm (f n)\<close>
+      by blast
+    then obtain n where \<open>y = onorm (f n)\<close> by blast
+    hence \<open>y \<ge> 0\<close>
+      by (simp add: \<open>\<forall>n. bounded_linear (f n)\<close> onorm_pos_le)  
+    thus ?thesis using \<open>\<forall> n. onorm (f n) \<le> M\<close>
+      by (simp add: \<open>y = onorm (f n)\<close>)      
+  qed
+  hence \<open>\<forall>y\<in>{onorm (f n) |n. True}. dist 0 y \<le> M\<close>
+    by simp
+  thus ?thesis unfolding bounded_def 
+    by blast
+qed
+
+(* non-boolean reformulation of Banach-Steinhaus theorem *)
+(* The Banach-Steinhaus theorem is interpreted as the fact that a type is inhabited *)
+lift_definition Banach_Steinhaus_real_bounded::
+\<open>('a::{banach,perfect_space}, 'b::real_normed_vector) real_bounded_SEQ_bounded_pointwise \<Rightarrow>
+real bounded_SEQ\<close> 
+(* proof *)
+is \<open>\<lambda> f. (\<lambda> n::nat. norm (f n))\<close>
+  apply transfer   
+  apply auto
+  using Banach_Steinhaus_coro
+  by auto
+(* qed *)
+
+
+(*
+
+(* NEW *)
+text \<open>The proof of the following result was taken from [sokal2011really]\<close>
+theorem Banach_Steinhaus:
+  fixes f :: \<open>'c \<Rightarrow> ('a::{banach,perfect_space} \<Rightarrow> 'b::real_normed_vector)\<close>
+  assumes \<open>\<And> n. bounded_linear (f n)\<close>
+    and  \<open>\<And> x. \<exists> M. \<forall> n.  norm ((f n) x) \<le> M\<close>
+  shows  \<open>\<exists> M. \<forall> n. onorm (f n) \<le> M\<close>
+
+
+*)
+
+subsection \<open>Convergence\<close>
+
+lift_definition strong_convergence_real_bounded:: "(nat \<Rightarrow> ('a::real_normed_vector, 'b::real_normed_vector) real_bounded) \<Rightarrow> (('a, 'b) real_bounded) \<Rightarrow> bool"
+  is \<open>\<lambda> f. \<lambda> l. f \<midarrow>strong\<rightarrow> l\<close>.
+
+abbreviation
+  strong_convergence_real_bounded_abbr :: "(nat \<Rightarrow> ('a::real_normed_vector, 'b::real_normed_vector) real_bounded) \<Rightarrow> (('a, 'b) real_bounded ) \<Rightarrow> bool"  ("((_)/ \<midarrow>STRONG\<rightarrow> (_))" [60, 60] 60)
+  where "f \<midarrow>STRONG\<rightarrow> l \<equiv> (strong_convergence_real_bounded f l ) "
+
+lift_definition onorm_convergence_real_bounded:: "(nat \<Rightarrow> ('a::real_normed_vector, 'b::real_normed_vector) real_bounded) \<Rightarrow> (('a, 'b) real_bounded) \<Rightarrow> bool"
+  is \<open>\<lambda> f. \<lambda> l. f \<midarrow>onorm\<rightarrow> l\<close>.
+
+abbreviation
+  onorm_convergence_real_bounded_abbr :: "(nat \<Rightarrow> ('a::real_normed_vector, 'b::real_normed_vector) real_bounded) \<Rightarrow> (('a, 'b) real_bounded ) \<Rightarrow> bool"  ("((_)/ \<midarrow>ONORM\<rightarrow> (_))" [60, 60] 60)
+  where "f \<midarrow>ONORM\<rightarrow> l \<equiv> (onorm_convergence_real_bounded f l ) "
 
 
 end
