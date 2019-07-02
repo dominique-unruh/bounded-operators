1f6a6e6295c42988c3b752d22cb905dc1f23c0a2
José Manuel Rodríguez Caballero <37361666+josephcmac@users.noreply.github.com>
2019-07-02 17:27:53 +0300

    progress in the proof of uniform_strong_onorm

--- SEQ_bounded_operators.thy
+++ SEQ_bounded_operators.thy
@@ -1252,7 +1252,7 @@ qed
 
 
 lemma uniform_strong_onorm:
-  fixes f :: \<open>nat \<Rightarrow> ('a::real_normed_vector \<Rightarrow> 'b::real_normed_vector)\<close>
+  fixes f :: \<open>nat \<Rightarrow> ('a::{real_normed_vector, perfect_space} \<Rightarrow> 'b::real_normed_vector)\<close>
     and l :: \<open>'a \<Rightarrow> 'b\<close>
   assumes \<open>\<forall>n. bounded_linear (f n)\<close> and \<open>bounded_linear l\<close>
     and \<open>f \<midarrow>ustrong\<rightarrow> l\<close>
@@ -1281,31 +1281,123 @@ proof-
             by simp
           moreover have \<open>\<bar>Sup {ereal (norm (g x)) | x. norm x = 1}\<bar> \<noteq> \<infinity>\<close>
           proof-
-            have \<open>\<exists> M::real. \<forall> x. norm x = 1 \<longrightarrow> \<bar> ereal (norm (g x)) \<bar> \<le> M\<close>
-              sorry
-            thus ?thesis  sorry
+            have \<open>\<exists> M::real. \<forall> x. norm x = 1 \<longrightarrow>  (norm (g x))  \<le> M\<close>
+              by (metis \<open>bounded_linear g\<close>  bounded_linear.bounded)
+            then obtain M::real where \<open>\<forall> x. norm x = 1 \<longrightarrow>  (norm (g x))  \<le> M\<close>
+              by blast
+            hence \<open>\<forall> x. norm x = 1 \<longrightarrow> ereal (norm (g x)) \<le> M\<close>
+              by simp
+            hence \<open>t \<in> {ereal (norm (g x)) | x. norm x = 1} \<Longrightarrow> t \<le> M\<close>
+              for t
+              by blast
+            hence \<open>Sup {ereal (norm (g x)) | x. norm x = 1} \<le> M\<close>
+              using Sup_least by blast
+            moreover have \<open>Sup { ereal (norm (g x))  | x. norm x = 1} \<ge> 0\<close>
+            proof-
+              have \<open>t \<in> { ereal (norm (g x))  | x. norm x = 1} \<Longrightarrow> t \<ge> 0\<close>
+                for t
+                by auto                
+              moreover have \<open>{ ereal (norm (g x))  | x. norm x = 1} \<noteq> {}\<close>
+              proof-
+                have \<open>\<exists> x::'a.  norm x = 1\<close>
+                  using le_numeral_extra(1) vector_choose_size by blast
+                thus ?thesis by blast
+              qed
+              ultimately show ?thesis
+                by (meson less_eq_Sup) 
+            qed
+            ultimately have \<open>\<bar> Sup { ereal (norm (g x))  | x. norm x = 1} \<bar> \<le> M\<close>
+              by simp
+            thus ?thesis by auto
           qed
           moreover have \<open>{ereal(norm (g x)) | x. norm x = 1} \<noteq> {}\<close>
             by (metis Sup_empty bot.extremum_strict calculation(2) less_ereal.simps(1) lt_ex not_infty_ereal)
-            
           ultimately have \<open>\<exists> t \<in> {ereal(norm (g x)) | x. norm x = 1}. Sup {ereal(norm (g x)) | x. norm x = 1}
                - ereal (inverse (real (Suc m))) < t\<close>
             by (rule Sup_ereal_close)
           hence \<open>\<exists> t \<in> {(norm (g x)) | x. norm x = 1}. Sup {ereal(norm (g x)) | x. norm x = 1}
                - (inverse (real (Suc m))) < t\<close>
             by auto
-          then obtain t where \<open>t \<in> {(norm (g x)) | x. norm x = 1}\<close> 
+          then obtain t::real where \<open>t \<in> {(norm (g x)) | x. norm x = 1}\<close> 
             and \<open>Sup {ereal(norm (g x)) | x. norm x = 1}
                - (inverse (real (Suc m))) < t\<close>
             by blast
           have \<open>onorm g = Sup {ereal(norm (g x)) | x. norm x = 1}\<close>
-            sorry
-          hence  \<open>onorm g - (inverse (real (Suc m))) < t\<close>
-            using  \<open>Sup {ereal(norm (g x)) | x. norm x = 1}
+            using \<open>onorm g = Sup {norm (g x) | x. norm x = 1}\<close>
+            by simp
+          also have \<open>...
+                < t + (inverse (real (Suc m)))\<close>
+            apply auto
+            using \<open>Sup {ereal(norm (g x)) | x. norm x = 1}
                - (inverse (real (Suc m))) < t\<close>
-            sorry
-          show ?thesis 
-            sorry
+          proof auto
+            assume \<open>Sup {ereal (norm (g x)) |x. norm x = 1} - ereal (inverse (1 + real m))
+                  < ereal t\<close>
+            moreover have \<open>\<bar> ereal (inverse (1 + real m)) \<bar> \<noteq> \<infinity>\<close>
+              by auto
+            ultimately have \<open>Sup {ereal (norm (g x)) |x. norm x = 1}
+                  < ereal t + ereal (inverse (1 + real m))\<close>
+              using ereal_minus_less by simp               
+            hence \<open>Sup {ereal (norm (g x)) |x. norm x = 1}
+                  < t + (inverse (1 + real m))\<close>
+              by simp
+            moreover have \<open>Sup {ereal (norm (g x)) |x. norm x = 1} = 
+                    Sup {(norm (g x)) |x. norm x = 1}\<close>
+            proof-
+              have \<open>{ereal (norm (g x)) |x. norm x = 1} = 
+                    {(norm (g x)) |x. norm x = 1}\<close>
+                by simp
+              hence \<open>y \<in> {ereal (norm (g x)) |x. norm x = 1}  
+                  \<Longrightarrow> y \<in> {(norm (g x)) |x. norm x = 1}\<close>
+                for y
+                by simp
+              moreover have \<open>{ (norm (g x)) |x. norm x = 1} \<noteq> {}\<close>
+                by (metis \<open>t \<in> {norm (g x) |x. norm x = 1}\<close> empty_iff)                
+              moreover have \<open>bdd_above { (norm (g x)) |x. norm x = 1}\<close>
+                by (metis (mono_tags, lifting)  \<open>bounded_linear g\<close> norm_set_bdd_above_eq1) 
+              ultimately have \<open>y \<in> {ereal (norm (g x)) |x. norm x = 1}  
+                  \<Longrightarrow> y \<le> Sup {(norm (g x)) |x. norm x = 1}\<close>
+                for y
+                by (smt cSup_upper ereal_less_eq(3) mem_Collect_eq)
+              from \<open>{ereal (norm (g x)) |x. norm x = 1} = 
+                    {(norm (g x)) |x. norm x = 1}\<close>
+              have \<open>y \<in> {(norm (g x)) |x. norm x = 1} \<Longrightarrow>
+                   y \<in> {ereal (norm (g x)) |x. norm x = 1}\<close>
+                for y
+                by simp
+              moreover have \<open>{ereal (norm (g x)) |x. norm x = 1} \<noteq> {}\<close>
+                by (metis \<open>{ereal (norm (g x)) |x. norm x = 1} \<noteq> {}\<close>)                
+              moreover have \<open>bdd_above {ereal (norm (g x)) |x. norm x = 1}\<close>
+                by (metis (no_types, lifting) bdd_above_top) 
+              ultimately have \<open>y \<in> {(norm (g x)) |x. norm x = 1}  
+                  \<Longrightarrow> y \<le> Sup {ereal (norm (g x)) |x. norm x = 1}\<close>
+                for y
+                by (simp add: Sup_upper)
+
+              from  \<open>\<And> y. y \<in> {ereal (norm (g x)) |x. norm x = 1}  
+                  \<Longrightarrow> y \<le> Sup {(norm (g x)) |x. norm x = 1}\<close>
+              have \<open>Sup {ereal (norm (g x)) |x. norm x = 1} \<le>  Sup {(norm (g x)) |x. norm x = 1}\<close>
+                using \<open>{ereal (norm (g x)) |x. norm x = 1} \<noteq> {}\<close>
+                by (meson cSup_least)
+
+              from  \<open>\<And> y. y \<in> { (norm (g x)) |x. norm x = 1}  
+                  \<Longrightarrow> y \<le> Sup {ereal (norm (g x)) |x. norm x = 1}\<close>
+              have \<open>(Sup { (norm (g x)) |x. norm x = 1}) \<le> Sup {ereal (norm (g x)) |x. norm x = 1}\<close>
+                sorry
+               
+              show ?thesis 
+                using \<open>(Sup {(norm (g x)) |x. norm x = 1}) \<le> Sup {ereal (norm (g x)) |x. norm x = 1}\<close>
+                 \<open>Sup {ereal (norm (g x)) |x. norm x = 1} \<le>  Sup {(norm (g x)) |x. norm x = 1}\<close>
+                by auto
+            qed
+            ultimately show \<open>Sup {norm (g x) |x. norm x = 1} < t + inverse (1 + real m)\<close>
+              by simp
+              
+          qed
+          finally have \<open>(onorm g) <  t + (inverse (real (Suc m)))\<close>
+            by blast
+          thus ?thesis
+            using \<open>t \<in> {norm (g x) |x. norm x = 1}\<close> by auto             
         qed
         thus ?thesis by auto
       qed
@@ -1328,8 +1420,7 @@ proof-
         then obtain x where \<open>norm (x m) = 1\<close> 
           and \<open>onorm (\<lambda> x. f n x - l x) \<le> norm ((\<lambda> x. f n x - l x) (x m)) + inverse (real (Suc m))\<close>
         for m::nat
-          by blast
-          
+          by blast          
         have \<open>\<forall> m. onorm (\<lambda> x. f n x - l x) < e + inverse (real (Suc m))\<close>
           using \<open>\<And> m. norm (x m) = 1\<close>  \<open>\<And> m. onorm (\<lambda> x. f n x - l x) \<le> norm ((\<lambda> x. f n x - l x) (x m)) + inverse (real (Suc m)) \<close>
                 \<open>\<forall> x. norm x = 1 \<longrightarrow> norm (f n x - l x) < e\<close>
