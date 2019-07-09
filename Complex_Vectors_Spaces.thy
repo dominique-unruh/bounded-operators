f6247a23ef51f38bb58bdd9c1aa41e68d13ddfb6
José Manuel Rodríguez Caballero <37361666+josephcmac@users.noreply.github.com>
2019-07-09 22:11:13 +0300

    Ordered_Fields revisited

---
+++ Complex_Vectors_Spaces.thy
@@ -0,0 +1,16 @@
+(*
+Authors: 
+
+  Dominique Unruh, University of Tartu, unruh@ut.ee
+  Jose Manuel Rodriguez Caballero, University of Tartu, jose.manuel.rodriguez.caballer@ut.ee
+*)
+
+
+theory Complex_Vectors_Spaces
+imports 
+  VectorSpace.VectorSpace
+begin
+
+
+
+end
