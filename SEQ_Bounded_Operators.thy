8607e8eb11c81ab866b70cb0e8ae766c521c0aa6
José Manuel Rodríguez Caballero <37361666+josephcmac@users.noreply.github.com>
2019-07-13 15:14:45 +0300

    reformulation of strong convergence on the unit sphere using filters

--- SEQ_Bounded_Operators.thy
+++ SEQ_Bounded_Operators.thy
@@ -54,6 +54,34 @@ definition uCauchy::
   \<open>(nat \<Rightarrow> ('a::real_normed_vector \<Rightarrow> 'b::real_normed_vector)) \<Rightarrow> bool\<close>
   where \<open>uCauchy f = ( \<forall> e > 0. \<exists>M. \<forall>m\<ge>M. \<forall>n\<ge>M. \<forall> x. norm x = 1 \<longrightarrow> norm (f m x - f n x) < e )\<close>
 
+section \<open>Topological translation\<close>
+subsection \<open>Strong convergence uniformly on the unit sphere\<close>
+
+definition nhds_ustrong:: \<open>('a::real_normed_vector\<Rightarrow>'b::real_normed_vector) \<Rightarrow> ('a\<Rightarrow>'b) filter\<close>
+  where \<open>nhds_ustrong l =
+ (INF e \<in> {(0::real)<..}. principal {f. \<forall> x. norm x = 1 \<longrightarrow> norm (f x - l x) < e })\<close>
+
+lemma ustrong_convergence_topo:
+  assumes \<open>f \<midarrow>ustrong\<rightarrow> l\<close>
+  shows \<open>filterlim f (nhds_ustrong l) sequentially\<close>
+proof-
+  have \<open>Abs_filter (\<lambda>P. \<forall>\<^sub>F x in sequentially. P (f x))
+    \<le> (INF e\<in>{0<..}. 
+  principal {f. \<forall>x. norm x = 1 \<longrightarrow> norm (f x - l x) < e})\<close>
+    sorry    
+  thus ?thesis unfolding filterlim_def filtermap_def nhds_ustrong_def
+    by blast
+qed
+
+lemma convergence_topo_ustrong:
+  assumes \<open>filterlim f (nhds_ustrong l) sequentially\<close> 
+  shows \<open>f \<midarrow>ustrong\<rightarrow> l\<close>
+  sorry
+
+proposition ustrong_convergence_topo_iff:
+\<open>( f \<midarrow>ustrong\<rightarrow> l ) \<longleftrightarrow> ( filterlim f (nhds_ustrong l) sequentially )\<close>
+  using convergence_topo_ustrong ustrong_convergence_topo by auto  
+
 section \<open>External analogs\<close>
 
 subsection \<open>nsustrong_convergence\<close>
