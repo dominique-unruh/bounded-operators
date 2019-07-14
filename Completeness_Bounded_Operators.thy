9e351ddd99c812ec256da0ac1215f1b3809c182a
José Manuel Rodríguez Caballero <37361666+josephcmac@users.noreply.github.com>
2019-07-14 16:50:09 +0300

    Update Completeness_Bounded_Operators.thy

--- Completeness_Bounded_Operators.thy
+++ Completeness_Bounded_Operators.thy
@@ -339,11 +339,11 @@ proof-
     using \<open>\<And> n. bounded_linear (f n)\<close>
       uCauchy_ustrong
     by auto
-  then obtain l where  \<open>bounded_linear l\<close> and \<open>f \<midarrow>ustrong\<rightarrow> l\<close> 
+  then obtain l where \<open>bounded_linear l\<close> and \<open>f \<midarrow>ustrong\<rightarrow> l\<close> 
     by blast
   have \<open>(\<lambda>n. onorm (\<lambda>x. f n x - l x)) \<longlonglongrightarrow> 0\<close>
-    using  \<open>f \<midarrow>ustrong\<rightarrow> l\<close>
-      \<open>bounded_linear l\<close> assms(1) onorm_convergence_def ustrong_onorm by blast
+    using  \<open>f \<midarrow>ustrong\<rightarrow> l\<close> \<open>bounded_linear l\<close> assms(1) onorm_convergence_def ustrong_onorm 
+    by blast
   thus ?thesis
     unfolding onorm_convergence_def using \<open>bounded_linear l\<close> by blast
 qed
