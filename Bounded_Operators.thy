f9732822fe4e40cec632758cee9204eb00cecac7
Dominique Unruh <unruh@ut.ee>
2019-07-15 11:51:10 +0300

    instantiation ell2 :: (singleton) complex_algebra_1. Defined addState more nicely.

--- Bounded_Operators.thy
+++ Bounded_Operators.thy
@@ -2031,20 +2031,72 @@ instance apply standard by auto
 end
 
 (* TODO move to L2_Complex *)
+instantiation ell2 :: (singleton) complex_algebra_1 begin
+lift_definition one_ell2 :: "'a ell2" is "\<lambda>_. 1" by simp
+lift_definition times_ell2 :: "'a ell2 \<Rightarrow> 'a ell2 \<Rightarrow> 'a ell2" is "\<lambda>a b _. a the_single * b the_single" by simp
+instance 
+proof
+  show "(a::'a ell2) * b * c = a * (b * c)"
+    for a :: "'a ell2"
+      and b :: "'a ell2"
+      and c :: "'a ell2"
+    by (transfer, auto)
+  show "((a::'a ell2) + b) * c = a * c + b * c"
+    for a :: "'a ell2"
+      and b :: "'a ell2"
+      and c :: "'a ell2"
+    apply (transfer, rule ext, auto)
+    by (simp add: distrib_left mult.commute)
+  show "(a::'a ell2) * (b + c) = a * b + a * c"
+    for a :: "'a ell2"
+      and b :: "'a ell2"
+      and c :: "'a ell2"
+    apply transfer apply (rule ext) apply auto
+    using distrib_left by blast
+  show "a *\<^sub>C (x::'a ell2) * y = a *\<^sub>C (x * y)"
+    for a :: complex
+      and x :: "'a ell2"
+      and y :: "'a ell2"
+    by (transfer, auto)
+  show "(x::'a ell2) * a *\<^sub>C y = a *\<^sub>C (x * y)"
+    for x :: "'a ell2"
+      and a :: complex
+      and y :: "'a ell2"
+    by (transfer, auto)
+  show "(1::'a ell2) * a = a"
+    for a :: "'a ell2"
+    apply (transfer, rule ext, auto simp: everything_the_single)
+    by (metis (full_types) everything_the_single)
+  show "(a::'a ell2) * 1 = a"
+    for a :: "'a ell2"
+    apply (transfer, rule ext, auto simp: everything_the_single)
+    by (metis (full_types) everything_the_single)
+  show "(0::'a ell2) \<noteq> 1"
+    apply transfer
+    by (meson zero_neq_one)
+qed
+end
+
+(* TODO move to L2_Complex *)
 lift_definition C1_to_complex :: "'a::singleton ell2 \<Rightarrow> complex" is
   "\<lambda>\<psi>. \<psi> the_single" .
-lift_definition complex_to_C1 :: "complex \<Rightarrow> 'a::singleton ell2" is
+abbreviation "complex_to_C1 :: complex \<Rightarrow> 'a::singleton ell2 == of_complex"
+
+(* lift_definition complex_to_C1 :: "complex \<Rightarrow> 'a::singleton ell2" is
   "\<lambda>c _. c" 
-  by simp
+  by simp *)
 
-lemma C1_to_complex_inverse[simp]: "complex_to_C1 (C1_to_complex \<psi>) = \<psi>"
-  apply transfer apply (rule singleton_ext) by auto
-
-lemma complex_to_C1_inverse[simp]: "C1_to_complex (complex_to_C1 \<psi>) = \<psi>"
+lemma C1_to_complex_one[simp]: "C1_to_complex 1 = 1"
   apply transfer by simp
 
+lemma C1_to_complex_inverse[simp]: "complex_to_C1 (C1_to_complex \<psi>) = \<psi>"
+  unfolding of_complex_def apply transfer apply (rule singleton_ext) by auto
+
+lemma complex_to_C1_inverse[simp]: "C1_to_complex (complex_to_C1 \<psi>) = \<psi>"
+  unfolding of_complex_def apply transfer by simp
+
 lemma bounded_clinear_complex_to_C1: "bounded_clinear complex_to_C1"
-  apply (rule bounded_clinear_intro[where K=1])
+  unfolding of_complex_def apply (rule bounded_clinear_intro[where K=1])
   by (transfer; auto simp: ell2_norm_finite_def)+
 
 lemma bounded_clinear_C1_to_complex: "bounded_clinear C1_to_complex"
@@ -2171,22 +2223,39 @@ lemma unitary_assoc_op[simp]: "unitary assoc_op"
 
 lemma tensor_scalar_mult1[simp]: "(a *\<^sub>C A) \<otimes> B = a *\<^sub>C (A \<otimes> B)" for a::complex and A::"('a,'b)l2bounded" and B::"('c,'d)l2bounded"
   by (cheat TODO3)
+lemma tensor_scalar_mult1_ell2[simp]: "(a *\<^sub>C A) \<otimes> B = a *\<^sub>C (A \<otimes> B)" for a::complex and A::"_ ell2" and B::"_ ell2"
+  by (cheat TODO3)
 lemma tensor_scalar_mult2[simp]: "A \<otimes> (a *\<^sub>C B) = a *\<^sub>C (A \<otimes> B)" for a::complex and A::"('a,'b)l2bounded" and B::"('c,'d)l2bounded"
   by (cheat TODO3)
+lemma tensor_scalar_mult2_ell2[simp]: "A \<otimes> (a *\<^sub>C B) = a *\<^sub>C (A \<otimes> B)" for a::complex and A::"_ ell2" and B::"_ ell2"
+  by (cheat TODO3)
+lemma tensor_plus: "(A+B) \<otimes> C = A \<otimes> C + B \<otimes> C" for A B C :: "(_,_)bounded"
+  by (cheat tensor_plus)
+lemma tensor_plus_ell2: "(A+B) \<otimes> C = A \<otimes> C + B \<otimes> C" for A B C :: "_ ell2"
+  by (cheat tensor_plus)
+lemma tensor_norm_ell2: "norm (\<psi> \<otimes> \<phi>) = norm \<psi> * norm \<phi>" for \<psi> \<phi> :: "_ ell2"
+  by (cheat tensor_norm_ell2)
 
 lemma tensor_times[simp]: "(U1 \<otimes> U2) \<cdot> (V1 \<otimes> V2) = (U1 \<cdot> V1) \<otimes> (U2 \<cdot> V2)"
   for V1 :: "('a1,'b1) l2bounded" and U1 :: "('b1,'c1) l2bounded"
     and V2 :: "('a2,'b2) l2bounded" and U2 :: "('b2,'c2) l2bounded"
   by (cheat TODO3)
 
+lift_definition addState :: "'a ell2 \<Rightarrow> ('b,'b*'a) l2bounded" is
+  \<open>\<lambda>\<psi> \<phi>. tensorVec \<phi> \<psi>\<close>
+  apply (rule_tac K="norm ell2" in bounded_clinear_intro)
+  by (auto simp: tensor_norm_ell2 tensor_plus_ell2)
+  
+
+(* TODO: this is simply the adjoint of addState (1::unit ell2), and addState y is best defined as x \<rightarrow> x \<otimes> y (lifted).
+   Do we even use remove_qvar_unit_op then? *)
 consts remove_qvar_unit_op :: "('a*unit,'a) l2bounded"
 
-
-definition addState :: "'a ell2 \<Rightarrow> ('b,'b*'a) l2bounded" where
-  "addState \<psi> = idOp \<otimes> (ell2_to_bounded \<psi>) \<cdot> remove_qvar_unit_op*"
+(* definition addState :: "'a ell2 \<Rightarrow> ('b,'b*'a) l2bounded" where
+  "addState \<psi> = idOp \<otimes> (ell2_to_bounded \<psi>) \<cdot> remove_qvar_unit_op*" *)
 
 lemma addState_times_scalar[simp]: "addState (a *\<^sub>C \<psi>) = a *\<^sub>C addState \<psi>" for a::complex and psi::"'a ell2"
-  unfolding addState_def by (simp add: ell2_to_bounded_scalar_times)
+  apply transfer by auto
 
 lemma tensor_adjoint[simp]: "adjoint (U\<otimes>V) = (adjoint U) \<otimes> (adjoint V)"
   for U :: "('a,'b) l2bounded" and V :: "('c,'d) l2bounded"
