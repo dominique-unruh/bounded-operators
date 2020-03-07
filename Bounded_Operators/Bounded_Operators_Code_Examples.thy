5c1e20ff6d4b5ec3c16016e1967cf44381dcb0e8
José Manuel Rodríguez Caballero <37361666+josephcmac@users.noreply.github.com>
2020-03-07 23:07:41 +0200

    update
    subsection ‹Complex vector spaces›

--- Bounded_Operators/Bounded_Operators_Code_Examples.thy
+++
@@ -1,9 +0,0 @@
-theory Bounded_Operators_Code_Examples
-  imports
-    Real_Impl.Real_Impl "HOL-Library.Code_Target_Numeral"
-    Bounded_Operators.Bounded_Operators_Code 
-begin
-
-value "idOp + idOp :: (bool ell2,bool ell2) bounded"
-
-end
