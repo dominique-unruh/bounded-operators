81bfdc5fabc75f6cf0e2bee4b73d13877d513e9e
Dominique Unruh <unruh@ut.ee>
2021-04-19 18:07:33 +0300

    Moved stuff from Preliminaries to Extra_General

--- extra/Preliminaries.thy
+++ extra/Preliminaries.thy
@@ -19,56 +19,11 @@ theory Preliminaries
     "HOL.Real_Vector_Spaces"
     "HOL-Analysis.Uniform_Limit"
 
+  Extra_General
 
 begin
 
 subsection\<open>General Results Missing\<close>
-
-class not_singleton =
-  assumes not_singleton_card: "\<exists>x y. x \<noteq> y"
-
-lemma not_singleton_existence[simp]:
-  \<open>\<exists> x::('a::not_singleton). x \<noteq> t\<close>
-proof (rule ccontr)
-  assume \<open>\<nexists>x::'a. x \<noteq> t\<close> 
-  have \<open>\<exists>x::'a. \<exists>y. x \<noteq> y\<close>
-    using not_singleton_card
-    by blast
-  then obtain x y::'a where \<open>x \<noteq> y\<close>
-    by blast
-  have \<open>\<forall>x::'a. x = t\<close>
-    using \<open>\<nexists>x::'a. x \<noteq> t\<close> by simp
-  hence \<open>x = t\<close>
-    by blast
-  moreover have \<open>y = t\<close>
-    using \<open>\<forall>x::'a. x = t\<close>
-    by blast
-  ultimately have \<open>x = y\<close>
-    by simp
-  thus False
-    using \<open>x \<noteq> y\<close> by auto 
-qed
-
-lemma UNIV_not_singleton[simp]: "(UNIV::_::not_singleton set) \<noteq> {x}"
-  using not_singleton_existence[of x] by blast
-
-lemma UNIV_not_singleton_converse: 
-  assumes"\<And>x::'a. UNIV \<noteq> {x}"
-  shows "\<exists>x::'a. \<exists>y. x \<noteq> y"
-  using assms
-  by fastforce 
-
-lemma UNIV_not_singleton_converse_zero: 
-  assumes "UNIV \<noteq> {0::'a::real_normed_vector}"
-  shows "\<exists>x::'a. \<exists>y. x \<noteq> y"
-  using UNIV_not_singleton_converse assms
-  by fastforce 
-
-subclass (in card2) not_singleton
-  apply standard using two_le_card
-  by (meson card_2_iff' obtain_subset_with_card_n)
-
-
 
 typedef 'a euclidean_space = "UNIV :: ('a \<Rightarrow> real) set" ..
 setup_lifting type_definition_euclidean_space
@@ -3942,26 +3897,6 @@ proof-
   thus ?thesis using \<open>r > 0\<close>
     by auto
 qed
-
-context CARD_1 begin
-
-lemma everything_the_same[simp]: "(x::'a)=y"
-  by (metis (full_types) UNIV_I card_1_singletonE empty_iff insert_iff local.CARD_1)
-
-lemma CARD_1_UNIV: "UNIV = {x::'a}"
-  by (metis (full_types) UNIV_I card_1_singletonE local.CARD_1 singletonD)
-
-lemma CARD_1_ext: "x (a::'a) = y b \<Longrightarrow> x = y"
-proof (rule ext)
-  show "x t = y t"
-    if "x a = y b"
-    for t :: 'a
-    using that  apply (subst (asm) everything_the_same[where x=a])
-    apply (subst (asm) everything_the_same[where x=b])
-    by simp
-qed 
-
-end
 
 subsection\<open>Unclassified\<close>
 
